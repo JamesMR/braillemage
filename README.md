c69cfbfbf77396d7c1fce18f6f8149608fccfea8
James Raymond <james@haiku.im>
2017-03-18 21:55:31 +0000

    Fix implementation of floyd-steinberg dithering, implement very basic CLI/module support

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # braillemage
 
+### Note: this is currently UNSTABLE, and more of a doodle than useable module yet
+
 A simple utility to convert images to a braille displayed format. Available as an imported module or command line interface
 
 ## TODO
--- index.js
+++ index.js
@@ -5,33 +5,49 @@ let w = process.stdout.columns * 2;
 let h = (process.stdout.rows-2) * 4;
 const c = new Canvas(w, h);
 
-JIMP.read('./sample_data/type.jpg')
-  .then((image) => {
-    let error = 0;
-    let e = {};
-    image.resize(150, 100)
-      .scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, i) => {
-        let n = `${x}.${y}`;
-        if (e[n]) error = e[n];
-      
-        const R = image.bitmap.data[i],
-          G = image.bitmap.data[i+1],
-          B = image.bitmap.data[i+2];
 
-        const b = (R+R+G+G+G+B)/6; 
+function renderImage(path){
+  return JIMP.read(path)
+    .then((image) => {
+      let error = 0;
+      let e = {};
+      let thisLine = [];
+      let nextLine = [0];
+      image.contain(w, h)
+        .scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, i) => {
+          if (x === 0) {
+            thisLine = nextLine;
+            nextLine = [];
+          }
 
-        v = b + error;
-        error -= v;
+          const R = image.bitmap.data[i],
+            G = image.bitmap.data[i+1],
+            B = image.bitmap.data[i+2];
 
-        error_divided = error/16;
-        e[`${x+1}.${y}`] = (e[`${x+1}.${y}`] || 0) + error_divided*7;
-        e[`${x-1}.${y+1}`] = (e[`${x-1}.${y+1}`] || 0) + error_divided*3;
-        e[`${x}.${y+1}`] = (e[`${x}.${y+1}`] || 0) + error_divided*5;
-        e[`${x+1}.${y+1}`] = (e[`${x+1}.${y+1}`] || 0) + error_divided*1;
+          const brightness = (R+G+G+G+B+B)/6; 
 
-        if (v > 0){
-          c.set(x, y);
-        }
-      });
-    console.log(c.frame());
-  });
+          v = brightness + error;
+          
+          const clamped = v > 128 ? 256 : 0;
+          error = v - clamped;
+
+          error_divided = error/16;
+          thisLine[x+1] = (thisLine[x+1] || 0) + error_divided*7;
+          nextLine[x-1] = (nextLine[x-1] || 0) + error_divided*3;
+          nextLine[x] = (nextLine[x] || 0) + error_divided*5;
+          nextLine[x+1] = (nextLine[x+1] || 0) + error_divided*1;
+
+          if (clamped > 128){
+            c.set(x, y);
+          }
+        });
+      return c.frame();
+    });
+}
+
+if (require.main === module) {
+  let path = process.argv.reverse()[0];
+  renderImage(path).then(img => console.log(img));
+} else {
+  module.exports = renderImage;
+}
